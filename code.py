fb665c21036d8cc7aaf3fecc0859044db7ff43c8
Samar Singh Holkar <samar.holkar@paytm.com>
2018-06-02 19:53:06 +0530

    Commenting the code

--- code.py
+++ code.py
@@ -1,16 +1,23 @@
 
+# Importing libraries
 import numpy as np
 import pandas as pd
 
-data_list = []
+# Importing the dataset
 training_data = pd.read_csv('train.csv')
 test_data = pd.read_csv('test.csv')
 y = training_data.iloc[:, 16].values
 training_data = training_data.iloc[:, :16]
+
+
+# Preprocessing the data
+data_list = []
 data_list.append(training_data)
 data_list.append(test_data)
 training_data = pd.concat(data_list)
-#training_data[0, 3, 4, 5, 6, 8, 9, 11, 12] = training_data[0, 3, 4, 5, 6, 8, 9, 11, 12].fillna(training_data[0, 3, 4, 5, 6, 8, 9, 11, 12].value_counts.index.values[0])
+
+
+# replacing missing categorical data with the most frequent data
 training_data['A'] = training_data['A'].fillna(training_data['A'].value_counts().index[0]) 
 training_data['D'] = training_data['D'].fillna(training_data['D'].value_counts().index[0])
 training_data['E'] = training_data['E'].fillna(training_data['E'].value_counts().index[0])
@@ -20,32 +27,25 @@ training_data['I'] = training_data['I'].fillna(training_data['I'].value_counts()
 training_data['J'] = training_data['J'].fillna(training_data['J'].value_counts().index[0])
 training_data['L'] = training_data['L'].fillna(training_data['L'].value_counts().index[0])
 training_data['M'] = training_data['M'].fillna(training_data['M'].value_counts().index[0])
-
-
 X = training_data.iloc[:, 1:16].values
-print(X.shape)
-
 X_num = X[:, [1, 2, 7, 10, 13, 14]]
-print(X.shape)
-print (X[551, 0])
-print (X[552, 0])
 
-#np.set_printoptions(precision=3, suppress=True)
-#for i in range(0, X_num.shape[1]-1):
-#   print(X_num[i])
-#   print(X_num[i].shape)
-#   np.genfromtxt(np.array(X_num[:,i]))
 
+
+# replacing missing numerical values with mean values of each columns
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X_num)
 X[:, [1, 2, 7, 10, 13, 14]] = imputer.transform(X_num)
 
 
+# feature scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X[:, [1, 2, 7, 10, 13, 14]] = sc.fit_transform(X[:, [1, 2, 7, 10, 13, 14]])
 
+
+# One-hot encoding categorical variables
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 label_encoders = [LabelEncoder() for i in range(9)]
 X[:, 0] = label_encoders[0].fit_transform(X[:, 0])
@@ -60,28 +60,29 @@ X[:, 12] = label_encoders[8].fit_transform(X[:, 12])
 onehotencoder = OneHotEncoder(categorical_features = [0, 3, 4, 5, 6, 8, 9, 11, 12])
 X = onehotencoder.fit_transform(X).toarray()
 
-print (X.shape)
+
+# separating training and testing data
 X_train = X[:552, :]
-print (X_train.shape)
 X_test = X[552:, :]
-print (X_test.shape)
 
 
-
+# Creating an xgboost model and finding the prediction
 from xgboost import XGBClassifier
 classifier = XGBClassifier(max_depth=3, min_child_weight=5, gamma = 0.0, subsample=0.8, colsample_bytree=0.6, reg_alpha=0.005, n_estimators=256, seed = 27)
 classifier.fit(X_train, y)
-
 y_pred = classifier.predict(X_test)
 
+
+# K-cross-validation
 from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y, cv = 20)
 print (accuracies.mean())
 print (accuracies.std())
 print (y_pred)
 
-from sklearn.model_selection import GridSearchCV
 
+# Appying grid search on each parameter of model in turns to find the optimal values of those parameters
+from sklearn.model_selection import GridSearchCV
 param_test6 = {
  'reg_lambda':[1e-2,0,0.0985, 0.09851, 0.099]
 }
@@ -93,6 +94,8 @@ gsearch1 = gsearch1.fit(X_train, y)
 best_val = gsearch1.grid_scores_, gsearch1.best_params_, gsearch1.best_score_
 print (best_val)
 
+
+# Creating the submission
 file = open("submission.csv","w")
 file.write("id,P\n")
 for i in range(0,y_pred.size):
